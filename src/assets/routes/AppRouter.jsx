b42d7c694365f83221fc43fcb19d561dba708c5a
Estefania-Catano <ecatano1@cesde.net>
2026-04-21 22:03:15 -0500

    feat: implementar navegación con react-router-dom y navbar

---
+++ src/assets/routes/AppRouter.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "../pages/Home";
+import Contacto from "../pages/Contacto";
+import Filosofia from "../pages/Filosofia";
+import Galeria from "../pages/Galeria";
+
+const AppRouter = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/filosofia" element={<Filosofia />} />
+        <Route path="/galeria" element={<Galeria />} />
+        <Route path="/contacto" element={<Contacto />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default AppRouter;
